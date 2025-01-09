83d0ef17bbe5ccaf691152228b560693f2883951
manishroy09 <manishroy2163@gmail.com>
2025-01-09 20:18:41 +0530

    9th jan 2025

--- .vscode/book.js
+++ .vscode/book.js
@@ -2,8 +2,8 @@ function low(){
     let con1 = document.getElementById('con1')
      let a = parseInt(prompt("who much set do you want? "))
      let final = document.getElementById('final')
-    
-     final.innerHTML = "your total amout = a*180 "
+     
+     final.innerHTML =  a*200;
 }
 
 
@@ -12,7 +12,7 @@ function high(){
      let a = parseInt(prompt("who much set do you want? "))
      let final = document.getElementById('final')
       
-      final.innerHTML = "your total amout = a*280"
+      final.innerHTML = a*280
      
 }
 
--- .vscode/book1.js
+++ .vscode/book1.js
@@ -2,7 +2,7 @@ function line1(){
     let a = parseInt(prompt('how much sets do you want to buy .'))
     
     let amount = document.getElementById('final')
-    amount.innerHTML =" <h1> Your total amout will be =  <h1>"(a*400)
+    amount.innerHTML =(a*400)
     document.body.style.backgroundColor = 'rgba(73, 12, 64, 0.808)'
     let icon = document.getElementsByTagName('i')
     icon.classList.add('icon')
@@ -12,14 +12,14 @@ function line1(){
 function center1(){
     let a = parseInt(prompt('how much sets do you want to buy .'))
     let amount = document.getElementById('final')
-    amount.innerHTML =" <h1> Your total amout will be = a*350 <h1>"
+    amount.innerHTML =a*350 
     document.body.style.backgroundColor = 'rgba(125, 38, 32, 0.808)'
 }
 
 function line2(){
     let a = parseInt(prompt('how much sets do you want to buy .'))
     let amount = document.getElementById('final')
-    amount.innerHTML =" <h1> Your total amout will be = a*250 <h1>"
+    amount.innerHTML =a*250 
     document.body.style.backgroundColor = 'rgba(52, 52, 13, 0.808)'
 }
 
@@ -27,14 +27,14 @@ function line2(){
 function line3(){
     let a = parseInt(prompt('how much sets do you want to buy .'))
     let amount = document.getElementById('final')
-    amount.innerHTML =" <h1> Your total amout will be = a*300 <h1>"
+    amount.innerHTML = a*300 
     document.body.style.backgroundColor =' rgba(14, 44, 26, 0.808)'
 }
 
 function center3(){
     let a = parseInt(prompt('how much sets do you want to buy .'))
     let amount = document.getElementById('final')
-    amount.innerHTML =" <h1> Your total amout will be = a*200 <h1>"
+    amount.innerHTML = a*200 
     document.body.style.backgroundColor = 'rgba(17, 43, 44, 0.808)'
 }
 
