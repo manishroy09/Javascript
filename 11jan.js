77cc9122987cca1927a483eeafe268349d865119
manishroy09 <manishroy2163@gmail.com>
2025-01-11 19:00:24 +0530

    11th jan 2025

---
+++ 11jan.js
@@ -0,0 +1,19 @@
+ 
+
+ function collor(){
+    let cont = document.getElementsByClassName('container')
+    cont[0].classList.toggle('colorr')
+ }
+
+
+// querySelector -- it will solve the problem of taking index when class is targated
+// querySelectorAll -- in this indexing is required 
+
+
+ let con1 = document.querySelector('.container1')
+ con1.style.color = 'blue'
+
+ let btn = document.querySelector('#btn')
+ btn.addEventListener('click', function(){
+   alert("add event fire")
+ })
