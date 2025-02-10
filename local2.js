99829751e6751e79ef384aa865929a3b7c60cd2e
manishroy09 <manishroy2163@gmail.com>
2025-02-10 19:26:21 +0530

    10th feb 2025

---
+++ local2.js
@@ -0,0 +1,27 @@
+
+
+const multi = () => {
+
+    let userdata = {
+        name : document.querySelector('#name').value,
+        email : document.querySelector('#email').value,
+        password : document.querySelector('#password').value
+    }
+
+    localStorage.setItem("userdata", JSON.stringify(userdata))
+}
+
+let data = JSON.parse(localStorage.getItem('userdata'))
+console.log(data); 
+
+function loginform(){
+    let logindata ={
+        loginemail : document.querySelector('#loginemail').value,
+        loginpassword : document.querySelector('#loginpass').value
+    }
+
+    if(data.email != logindata.loginemail || data.password != logindata.loginpassword){
+        alert("user not found");
+        return false;
+    }
+}
