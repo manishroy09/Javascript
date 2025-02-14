50ee19d4fed1b6b662b53ed6e6e79d9ea162a007
manishroy09 <manishroy2163@gmail.com>
2025-02-14 19:19:56 +0530

    14th feb 2025

--- json/data.js
+++ json/data.js
@@ -1,4 +1,4 @@
-async function fetdata() {
+   async function fetdata() {
     let data = await fetch('http://localhost:3000/student')
     let fdata = await data.json();
     let tdata = fdata.map((e)=>`
@@ -8,6 +8,7 @@ async function fetdata() {
     <td>${e.age}</td>
     <td>${e.contact}</td>
     <td>${e.city}</td>
+    <td> <button onclick="mydel('${e.id}')"> Delete </button></td>
     </tr>
     `
 ).join("")
@@ -16,3 +17,28 @@ document.getElementById('display').innerHTML = tdata;
 }
 
 fetdata();
+
+function mydel(id){
+    fetch(`http://localhost:3000/student/${id}`,{
+        method:'DELETE'
+        })
+    .then(r=>alert("Delete successfuly....!!!!"))
+
+}
+
+function insertdata(){
+    let frmdata = {
+        name:document.querySelector('#name').value,
+        age:document.querySelector('#age').value,
+        contact:document.querySelector('#contact').value,
+        city:document.querySelector('#city').value,
+    }
+    fetch('http://localhost:3000/student',{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(frmdata)
+    })
+    .then(r=>alert("data inserted....!!!!"))
+}
