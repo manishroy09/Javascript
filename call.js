26f10ce71701f1d92e41fd8eddf9813fd26fae8a
manishroy09 <manishroy2163@gmail.com>
2025-04-12 09:49:31 +0530

    12th april 2025

--- call.js
+++ call.js
@@ -22,5 +22,11 @@ let emp = [
 let data = emp.map((e)=>{return e.name})
 console.log(data);
 
+let ndata = emp.map((e)=>{return e.age>21})
+console.log(ndata)
+
+let hdata = emp.filter((e)=>{return e.name})
+console.log(hdata);
+
 let fdata = emp.filter((e)=>{return e.age>21})
 console.log(fdata);
