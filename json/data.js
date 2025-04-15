892a4723db9a56236102e0bd4c369edeab04eb78
manishroy09 <manishroy2163@gmail.com>
2025-04-15 13:04:15 +0530

    15th april 2025

--- json/data.js
+++ json/data.js
@@ -9,6 +9,7 @@
     <td>${e.contact}</td>
     <td>${e.city}</td>
     <td> <button onclick="mydel('${e.id}')"> Delete </button></td>
+    <td><button onclick= "myedit( '${e.id}') "> EDIT </button> </td>
     </tr>
     `
 ).join("")
@@ -41,4 +42,41 @@ function insertdata(){
         body:JSON.stringify(frmdata)
     })
     .then(r=>alert("data inserted....!!!!"))
+}
+
+async function myedit(id) {
+
+    let edata = await fetch(`http://localhost:3000/student/${id}`)
+    let fdata = await edata.json()
+
+    let frm = `
+    <input type="text" value=" ${fdata.name}" id="name"> <br><br>
+    <input type="text" value=" ${fdata.age}" id="age"> <br><br>
+    <input type="text" value=" ${fdata.contact}" id="contact"> <br><br>
+    <input type="text" value=" ${fdata.city}" id="city"> <br><br>
+
+    <input type="submit" onclick = "finalupdate('${fdata.id}')">
+    `
+   document.querySelector('#showedit').innerHTML =frm ;
+}
+
+function finalupdate(id) {
+
+    let final = {
+
+        name:document.querySelector('#name').value,
+        age:document.querySelector('#age').value,
+        contact:document.querySelector('#contact').value,
+        city:document.querySelector('#city').value
+    }
+
+    fetch(`http://localhost:3000/student/${id}`, {
+  
+        method:'PUT', 
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify(final)
+    })
+    .then(r=>alert ("data update sucsses"))
 }
