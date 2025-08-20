9ad5498f24e6ae92a587c0239b1ccc981a54e64b
manishroy09 <manishroy2163@gmail.com>
2025-08-20 17:37:48 +0530

    20th aug 2025

--- task1.js
+++ task1.js
@@ -8,15 +8,18 @@ console.log(r*3.14*r)// Math.PI*Math.POW(r,2)
 // to squar of a number
 let n=parseInt(prompt("Enter any number"))
 document.write(n*n)
+document.write(" ")
 
 //to swap two number
 
 let num1=parseInt(prompt("Enter first number"))
 let num2=parseInt(prompt("Enter second  number"))
 
+
 let num3=num2;
  num2=num1;
+ num1 = num3;
 
-document.write(num3)
+document.write(num1)
 document.write(num2) 
  
